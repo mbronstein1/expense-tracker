746ebc81e1edae3c0adfa8c4d7e6f1346ac977a4
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-10 16:27:13 -0600

    formatting/stylizing date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,14 @@
-import React from "react";
-import "./ExpenseItem.css";
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
 const ExpenseItem = ({ item: { title, amount, date } }) => {
-  const month = date.toLocaleString("en-US", { month: "short" });
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const year = date.getFullYear();
-
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <div className='expense-item__price'>${amount.toFixed(2)}</div>
       </div>
     </div>
   );
