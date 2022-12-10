c84b63f9e3369df6522c1786ae864516d7ee1c53
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-10 16:09:14 -0600

    formatting dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ item: { title, amount, date } }) => {
+  const month = date.toLocaleString("en-US", { month: "short" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className='expense-item'>
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
