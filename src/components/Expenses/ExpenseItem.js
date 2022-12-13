0b0915bea1e004f1f55ef82e82dd630c818aa22b
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-13 15:34:34 -0600

    cleaned ExpenseItem component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ item: { title, amount, date } }) => {
-  const [expenseTitle, setExpenseTitle] = useState(title);
-
-  const clickHandler = () => {
-    setExpenseTitle('Updated!!');
-  };
-
   return (
     <Card className='expense-item'>
       <ExpenseDate date={date} />
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${amount.toFixed(2)}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
