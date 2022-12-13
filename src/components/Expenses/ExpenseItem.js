1b51caa679bb80aa922d025392a5c4eac40b571e
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-12 20:32:56 -0600

    added expense form layout

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ item: { title, amount, date } }) => {
+  const [expenseTitle, setExpenseTitle] = useState(title);
+
+  const clickHandler = () => {
+    setExpenseTitle('Updated!!');
+  };
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className='expense-item__price'>${amount.toFixed(2)}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label htmlFor='expense-title'>Title</label>
+          <input
+            id='expense-title'
+            type='text'
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label htmlFor='expense-amount'>Amount</label>
+          <input
+            id='expense-amount'
+            type='number'
+            min='0.01'
+            step='0.01'
+          />
+        </div>
+        <div className='new-expense__control'>
+          <label htmlFor='expense-date'>Date</label>
+          <input
+            id='expense-date'
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+          />
+        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
