f6510f33ff7385f6fe1c6537eb62b99ae4baf572
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-12 21:22:14 -0600

    added onChange handler and formState object for form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  // instead of creating 3 different states for each value below, I created one object with each input as a key
+  const [formState, setFormState] = useState({ title: '', amount: '', date: '' });
+
+  // instead of creating three handleChange functions, I created one that sets the formState object based on e.target.name and e.target.value (ex: the first input e.target.name = title and e.target.value = 'this value', so the state for the title key is 'title': 'this value)
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   return (
     <form>
       <div className='new-expense__controls'>
@@ -10,6 +22,9 @@ const ExpenseForm = () => {
           <input
             id='expense-title'
             type='text'
+            onChange={changeHandler}
+            name='title'
+            value={formState.title}
           />
         </div>
         <div className='new-expense__control'>
@@ -19,6 +34,9 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step='0.01'
+            onChange={changeHandler}
+            name='amount'
+            value={formState.amount}
           />
         </div>
         <div className='new-expense__control'>
@@ -28,6 +46,9 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max='2022-12-31'
+            onChange={changeHandler}
+            name='date'
+            value={formState.date}
           />
         </div>
       </div>
