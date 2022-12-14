0b20cbab7f385cc6bb158705e2b04d30afff884c
Matthew Bronstein <mbronstein1@gmail.com>
2022-12-13 20:02:45 -0600

    added logic to show or hide ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,19 +1,13 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = ({ onSaveExpenseData }) => {
+const ExpenseForm = ({ onSaveExpenseData, setShowForm }) => {
   // instead of creating 3 different states for each value below, I created one object with each input as a key
   const [expenseData, setExpenseData] = useState({ title: '', amount: '', date: '' });
 
   // instead of creating three handleChange functions, I created one that sets the formState object based on e.target.name and e.target.value (ex: the first input e.target.name = title and e.target.value = 'this value', so the state for the title key is 'title': 'this value)
   const changeHandler = (e) => {
     const { name, value } = e.target;
-
-    // Less correct to call spread of state itself
-    // setExpenseData({
-    //   ...expenseData,
-    //   [name]: value,
-    // });
 
     // More correct to pass previous state in callback function and call spread of previous state
     setExpenseData((prevState) => {
@@ -27,6 +21,7 @@ const ExpenseForm = ({ onSaveExpenseData }) => {
   const submitHandler = (e) => {
     e.preventDefault();
     onSaveExpenseData({ ...expenseData, amount: +expenseData.amount, date: new Date(expenseData.date) });
+    setShowForm(false);
     setExpenseData({ title: '', amount: '', date: '' });
   };
 
@@ -69,6 +64,11 @@ const ExpenseForm = ({ onSaveExpenseData }) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button
+          type='button'
+          onClick={() => setShowForm(false)}>
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = ({ onAddExpense }) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -13,7 +15,18 @@ const NewExpense = ({ onAddExpense }) => {
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {showForm ? (
+        <ExpenseForm
+          setShowForm={setShowForm}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
+      ) : (
+        <button
+          type='button'
+          onClick={() => setShowForm(true)}>
+          Add New Expense
+        </button>
+      )}
     </div>
   );
 };
